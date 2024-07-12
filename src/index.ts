895101022006f71f55b84b8d45b36293d0b2bc80
Alex <aesshoferi@gmail.com>
2024-07-12 16:39:05 +0330

    fix: move types, rename some, and improve code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-export * from "./components/stepper.component";
+export { Stepper } from "./lib/stepper.component";
+export {
+  type DividerProps,
+  type Step,
+  type StepItemClassNames,
+  type StepItemProps,
+  type StepperClassNames,
+  type StepperProps,
+} from "./lib/types";
