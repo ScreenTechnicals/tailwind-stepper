05815becce10dbf0828e05a85a55caeb52d3ca83
Chinmay Sa <68277286+ScreenTechnicals@users.noreply.github.com>
2024-10-26 11:04:00 +0530

    test

--- src/lib/stepper.component.tsx
+++ src/lib/stepper.component.tsx
@@ -1,32 +1,22 @@
 import * as React from "react";
-import { useCallback, useMemo } from "react";
+import { useMemo } from "react";
 import { twJoin, twMerge } from "tailwind-merge";
-import { DividerProps, StepItemProps, StepperProps } from "./types";
 
-const GradientDivider = ({ orientation, className }: DividerProps) => (
-  <div
-    className={twMerge(
-      "w-5 md:min-w-20 h-px bg-gradient-to-r dark:from-white/50 dark:via-white/50 dark:to-white/50 light:from-black/50 light:via-black/50 light:to-black/50",
-      orientation === "vertical" && "my-2 w-px md:min-w-px md:min-h-20 h-5",
-      className
-    )}
-  />
-);
-const x = "hello";
-
-const DashedDivider = ({ orientation, className }: DividerProps) => {
+const DashedDivider = ({ orientation, className }: any) => {
   const dividers = useMemo(
     () =>
-      Array.from({ length: 6 }).map((_, index) => (
-        <GradientDivider
-          key={index}
-          className={twMerge(
-            "!min-w-1",
-            orientation === "vertical" && "rotate-90",
-            className
-          )}
-        />
-      )),
+      Array.from({ length: 6 }).map(
+        (_, index) =>
+          // <GradientDivider
+          //   key={index}
+          //   className={twMerge(
+          //     "!min-w-1",
+          //     orientation === "vertical" && "rotate-90",
+          //     className
+          //   )}
+          // />
+          "hello"
+      ),
     [orientation, className]
   );
 
@@ -41,12 +31,7 @@ const DashedDivider = ({ orientation, className }: DividerProps) => {
   );
 };
 
-const StepItem = ({
-  step,
-  isSelected,
-  classNames,
-  hideLabel,
-}: StepItemProps) => (
+const StepItem = ({ step, isSelected, classNames, hideLabel }: any) => (
   <button
     type="button"
     onClick={step.onClick}
@@ -73,23 +58,14 @@ const StepItem = ({
     >
       <span
         className={twMerge(
-          "bg-slate-900 ring-slate-800 md:text-base text-xs ring-4 rounded-full w-4 h-4 md:w-6 md:h-6 flex items-center justify-center",
-          classNames?.icon || ""
+          "md:text-base text-xs",
+          classNames?.label,
+          hideLabel && "hidden"
         )}
       >
-        {step.icon || step.step + 1}
+        {step.label}
       </span>
-      {isSelected && (
-        <span
-          className={twMerge(
-            "md:text-base text-xs",
-            classNames?.label,
-            hideLabel && "hidden"
-          )}
-        >
-          {step.label}
-        </span>
-      )}
+      {/* )} */}
     </span>
   </button>
 );
@@ -100,53 +76,53 @@ export const Stepper = ({
   orientation = "horizontal",
   hideLabel,
   classNames,
-}: StepperProps) => {
+}: any) => {
   const { divider, base, ...restClassName } = classNames || { divider: "" };
 
-  const renderSteps = useCallback(
-    () =>
-      steps.map(({ step, ...restStepProps }, index) => {
-        const isLastStep = steps.length - 1 === index;
-        const shouldShowSteps =
-          selectedStep <= 2 ? step <= 2 || isLastStep : step <= 1 || isLastStep;
-        const shouldShowDivider =
-          steps.length === 4 ? !isLastStep : step <= 1 && !isLastStep;
+  // const renderSteps = useCallback(
+  //   () =>
+  //     steps.map(({ step, ...restStepProps }, index) => {
+  //       const isLastStep = steps.length - 1 === index;
+  //       const shouldShowSteps =
+  //         selectedStep <= 2 ? step <= 2 || isLastStep : step <= 1 || isLastStep;
+  //       const shouldShowDivider =
+  //         steps.length === 4 ? !isLastStep : step <= 1 && !isLastStep;
 
-        return (
-          <div
-            key={step}
-            className={twJoin(
-              "flex items-center",
-              orientation === "vertical" && "flex-col"
-            )}
-          >
-            {steps.length > 4 && isLastStep && (
-              <DashedDivider orientation={orientation} className={divider} />
-            )}
-            {shouldShowSteps && (
-              <StepItem
-                step={{ step, ...restStepProps }}
-                isSelected={step === selectedStep}
-                classNames={restClassName}
-                hideLabel={hideLabel}
-              />
-            )}
-            {selectedStep === step && selectedStep > 2 && (
-              <StepItem
-                step={{ step, ...restStepProps }}
-                isSelected={step === selectedStep}
-                classNames={restClassName}
-                hideLabel={hideLabel}
-              />
-            )}
-            {shouldShowDivider && (
-              <GradientDivider orientation={orientation} className={divider} />
-            )}
-          </div>
-        );
-      }),
-    [steps, selectedStep, orientation, divider, restClassName, hideLabel]
-  );
+  //       return (
+  //         <div
+  //           key={step}
+  //           className={twJoin(
+  //             "flex items-center",
+  //             orientation === "vertical" && "flex-col"
+  //           )}
+  //         >
+  //           {/* {steps.length > 4 && isLastStep && (
+  //             <DashedDivider orientation={orientation} className={divider} />
+  //           )} */}
+  //           {shouldShowSteps && (
+  //             <StepItem
+  //               step={{ step, ...restStepProps }}
+  //               isSelected={step === selectedStep}
+  //               classNames={restClassName}
+  //               hideLabel={hideLabel}
+  //             />
+  //           )}
+  //           {selectedStep === step && selectedStep > 2 && (
+  //             <StepItem
+  //               step={{ step, ...restStepProps }}
+  //               isSelected={step === selectedStep}
+  //               classNames={restClassName}
+  //               hideLabel={hideLabel}
+  //             />
+  //           )}
+  //           {/* {shouldShowDivider && (
+  //             <GradientDivider orientation={orientation} className={divider} />
+  //           )} */}
+  //         </div>
+  //       );
+  //     }),
+  //   [steps, selectedStep, orientation, divider, restClassName, hideLabel]
+  // );
 
   return (
     <div
@@ -162,9 +138,9 @@ export const Stepper = ({
           orientation === "vertical" && "flex-col"
         )}
       >
-        {renderSteps()}
+        {/* {renderSteps()} */}
       </div>
-      {steps.find(({ step }) => step === selectedStep)?.content}
+      {/* {steps.find(({ step }) => step === selectedStep)?.content} */}
     </div>
   );
 };
