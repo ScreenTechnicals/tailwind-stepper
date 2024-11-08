1045d475f3340c5529b2154dea6ead5e6b19c196
Chinmay Sa <68277286+ScreenTechnicals@users.noreply.github.com>
2024-11-08 21:20:19 +0530

    refactor: comment out unused components and type definitions in stepper component and types.ts

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,28 +1,28 @@
 import { ComponentProps } from "react";
 
-export type Step = Pick<ComponentProps<"button">, "onClick"> & {
-  step: number;
-  label: string;
-  //   icon?: ReactNode;
-  //   content?: ReactNode;
-  // };
+// export type Step = Pick<ComponentProps<"button">, "onClick"> & {
+//   step: number;
+//   label: string;
+//   //   icon?: ReactNode;
+//   //   content?: ReactNode;
+//   // };
 
-  // export type StepperClassNames = Record<
-  //   "wrapper" | "icon" | "label" | "gradientBorder" | "base" | "divider",
-  //   string
-  // >;
+//   // export type StepperClassNames = Record<
+//   //   "wrapper" | "icon" | "label" | "gradientBorder" | "base" | "divider",
+//   //   string
+//   // >;
 
-  export type StepItemClassNames = Record<
-    "wrapper" | "icon" | "label" | "gradientBorder",
-    string
-  >;
+//   export type StepItemClassNames = Record<
+//     "wrapper" | "icon" | "label" | "gradientBorder",
+//     string
+//   >;
 
-  export type StepItemProps = {
-    step: Step;
-    isSelected: boolean;
-    classNames?: Partial<StepItemClassNames>;
-    hideLabel?: boolean;
-  };
+//   export type StepItemProps = {
+//     step: Step;
+//     isSelected: boolean;
+//     classNames?: Partial<StepItemClassNames>;
+//     hideLabel?: boolean;
+//   };
 
   // export type StepperProps = {
   //   selectedStep: number;
